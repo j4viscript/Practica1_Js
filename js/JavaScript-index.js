c640415976920b0e9cdeb67ad1034b4934a15978
Javier Murillo <tezjavi@gmail.com>
2021-10-27 22:18:06 -0600

    Init Commit

---
+++ js/JavaScript-index.js
@@ -0,0 +1,15 @@
+var sign = prompt("What's your name?");
+alert("Hello " + sign + " welcome to the Jungle!!");
+
+var age = prompt("What's your age?");
+
+if(age>=18){
+
+    alert("Congratulations, you are "+ age + " years old, you can drive!")
+}
+else{
+    alert("You can't drive you are " + age + "years old.")
+}
+
+
+
